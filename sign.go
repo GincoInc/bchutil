20e6fe2f7ba592758426af368adffdd88883a3e0
ryokubozono <snusmumrik72@gmail.com>
2025-06-17 18:20:08 +0900

    Modify: adapt btcd v0.24.2

--- sign.go
+++ sign.go
@@ -5,7 +5,8 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"github.com/btcsuite/btcd/btcec"
+
+	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
